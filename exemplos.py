1476b4242f0a5c20bdf1fcfc57b231db0072ecc6
knzzinzx <ana.sanches.freitas@escola.pr.gov.br>
2026-02-12 10:51:57 -0300

    aai

--- exemplos.py
+++ exemplos.py
@@ -1,5 +1,15 @@
-print("olá mundo")
-import this
-print (7+4)
-print ( "7" + "4")
+# variavel é o nome que se da para algo ou coisa especifica, evita ter que digitar novamente toda vez que precisar alterar. este é um comentario
+faturamento = 1000
+custo = 700
+Lucro = 700-1000
 
+print("o faturamento foi de",1000-700)
+# As aspas só servem para texto, e virgula para inserir o numero
+
+#int= numeros inteiros
+
+#booleano = descobre se é verdadeiro ou falso 
+
+#numeros com casa decimal é o float
+
+teve_lucro= True
